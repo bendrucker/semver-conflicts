b332506cbfe0b71e623412a87dd9f58e9f2a0e6b
Ben Drucker <bvdrucker@gmail.com>
2015-06-06 11:33:34 -0400

    comments

--- index.js
+++ index.js
@@ -8,29 +8,30 @@ module.exports = function removeSemverConflicts (range) {
   return semver.Range(range)
     .set
     .map(sortComparators)
-    .map(cleanSet)
-    .map(function (comparators) {
-      return comparators.map(function (comparator) {
-        return comparator.toString()
-      })
-      .join(' ')
-    })
+    .map(dedupeComparators)
+    .map(stringifyComparators)
     .join(' || ')
 }
 
-function cleanSet (comparators) {
+function dedupeComparators (comparators) {
   return comparators.reduce(function (acc, current, index) {
+    // iterate over an array that excludes the current comparator
     var isDuplicate = remove(comparators, index)
       .some(function (comparator) {
+        // if there's no operator, see if the semver would satisfy the
+        // comparator under test
         if (!current.operator) {
           return comparator.test(current.semver)
         }
+        // both same direction (less than, greater than), maybe lte/gte
         if (current.operator.charAt(0) === comparator.operator.charAt(0)) {
           if (current.semver.version === comparator.semver.version) {
             if (current.operator === comparator.operator) {
+              // for identical comparators, just pick the latter one
               return index > comparators.indexOf(comparator)
             }
-            if (/=/.test(current.operator)) return true
+            // lt beats lte, gt beats gte
+            return /=/.test(current.operator)
           }
           var test = current.operator.charAt(0) === '>' ? 'lt' : 'gt'
           return semver[test](current.semver.toString(), comparator.semver.toString())
@@ -39,4 +40,11 @@ function cleanSet (comparators) {
     if (!isDuplicate) acc.push(current)
     return acc
   }, [])
+}
+
+function stringifyComparators (comparators) {
+  return comparators.map(function (comparator) {
+    return comparator.toString()
+  })
+  .join(' ')
 }
